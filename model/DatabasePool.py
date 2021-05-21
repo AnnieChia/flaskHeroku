ba8b6ec02c8b1df7454a08ef50cf3bd5fb193156
Annie <meoweng7@gmail.com>
2021-05-21 14:09:52 +0800

    V1 app

---
+++ model/DatabasePool.py
@@ -0,0 +1,33 @@
+from mysql.connector import Error
+from mysql.connector import pooling
+
+
+class DatabasePool:
+    #class variable
+    connection_pool = pooling.MySQLConnectionPool(pool_name="ws_pool",
+                                                  pool_size=5,
+                                                  host='localhost',
+                                                  database='furniture',
+                                                  user='root',
+                                                  password='Password@2021')
+
+
+    @classmethod
+    def getConnection(cls):
+        dbConn = cls.connection_pool.get_connection()
+        return dbConn
+
+
+'''
+connection_object=DatabasePool.getConnection()
+db_Info = connection_object.get_server_info()
+
+print("Connected to MySQL database using connection pool ... MySQL Server version on ", db_Info)
+
+cursor = connection_object.cursor(dictionary=True)
+
+cursor.execute("select * from user;")
+records = cursor.fetchall()
+for record in records:
+    print(record)
+'''
